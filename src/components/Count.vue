5c0c97fcdd9506515a3cb5e04812761240e303b3
lijianhu1 <403056352@qq.com>
2019-08-15 17:42:32 +0800

    初始化项目和添加demo

---
+++ src/components/Count.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="count-page">
+    <h5>我是子组件</h5>
+    <p>父组件的值：{{countNum}}</p>
+    <div class="count">
+      <el-button type="danger" icon="el-icon-minus" circle @click="countBtn(0)"></el-button>
+      <span class="num">{{countVal}}</span>
+      <el-button type="success" icon="el-icon-plus" circle @click="countBtn(1)"></el-button>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts">
+    import {Vue, Component, Prop} from "vue-property-decorator";
+
+    @Component
+    export default class Count extends Vue {
+        @Prop()
+        private countNum!: number;
+        private countVal: number = 0;
+
+        private countBtn(type: number) {
+            if (type) {
+                this.countVal += 1;
+            } else {
+                this.countVal -= 1;
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+  .count-page {
+    border: 1px solid #e38eff;
+  }
+
+  .count {
+    width: 150px;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+    text-align: center;
+    .num {
+      width: 30px;
+      height: 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border: 1px solid #999;
+      margin: 0 10px;
+    }
+  }
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="test-wrap">
+    <div class="test1">
+      <el-input v-model="text"></el-input>
+      <p>input文字为：{{text}}</p>
+    </div>
+    <div class="test2">
+      <h5>我是父组件</h5>
+      <div class="parent-box">
+        <span>父组件输入框：</span>
+        <el-input v-model.number="parentCount"></el-input>
+      </div>
+      <Count :countNum="parentCount"></Count>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator';
+  import Count from '../components/Count.vue';
+  @Component({
+      components: {
+          Count,
+      },
+  })
+  export default class Test extends Vue {
+      private text: string = '';
+      private parentCount: number = 0;
+  }
+</script>
+
+<style scoped lang="scss">
+  .test-wrap{
+    >div{
+      padding: 30px 0;
+      margin:20px 0;
+      border: 1px solid #999;
+    }
+  }
+  .test1{
+    width: 300px;
+    margin: 20px auto;
+  }
+  .parent-box{
+    width: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    span{
+      display: inline-block;
+      width: 150px;
+    }
+    .el-input{
+      width: 150px;
+    }
+  }
+</style>
